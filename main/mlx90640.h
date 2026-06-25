1987ad61a27bf3ae8ac729c53a5a021eb20de5c1
semtex <semtex@users.noreply.github.com>
2026-06-25 17:58:28 +0200

    feat: add mlx90640 public driver header

---
+++ main/mlx90640.h
@@ -0,0 +1,62 @@
+/*
+ * MLX90640 thermal array driver for ESP-IDF (new i2c_master API)
+ * 32x24 IR array, I2C interface.
+ */
+#ifndef MLX90640_H
+#define MLX90640_H
+
+#include <stdint.h>
+#include "esp_err.h"
+#include "driver/i2c_master.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MLX90640_ROWS 24
+#define MLX90640_COLS 32
+#define MLX90640_PIXELS (MLX90640_ROWS * MLX90640_COLS)
+
+#define MLX90640_DEFAULT_ADDR 0x33
+
+typedef struct {
+    i2c_master_dev_handle_t dev;
+    uint8_t addr;
+    /* Calibration parameters extracted from EEPROM */
+    int16_t kVdd;
+    int16_t vdd25;
+    float KvPTAT;
+    float KtPTAT;
+    uint16_t vPTAT25;
+    float alphaPTAT;
+    int16_t gainEE;
+    float tgc;
+    float cpKv;
+    float cpKta;
+    uint8_t resolutionEE;
+    uint8_t calibrationModeEE;
+    float KsTa;
+    float ksTo[4];
+    int16_t ct[4];
+    float alpha[MLX90640_PIXELS];
+    int16_t offset[MLX90640_PIXELS];
+    float kta[MLX90640_PIXELS];
+    float kv[MLX90640_PIXELS];
+    float cpAlpha[2];
+    int16_t cpOffset[2];
+    float ilChessC[3];
+    uint16_t brokenPixels[5];
+    uint16_t outlierPixels[5];
+} mlx90640_t;
+
+/* Attach the sensor at `addr` to an existing I2C master bus and read EEPROM params. */
+esp_err_t mlx90640_init(i2c_master_bus_handle_t bus, uint8_t addr, mlx90640_t *out);
+
+/* Read one full frame and convert to Celsius in `out_temps[768]`. */
+esp_err_t mlx90640_read_frame(mlx90640_t *s, float out_temps[MLX90640_PIXELS]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MLX90640_H */
